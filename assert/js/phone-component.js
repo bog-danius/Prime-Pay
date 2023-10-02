8514e9f8278df068676280c9e5c88bb0c8816dcc
jenesius <lokargenia@gmail.com>
2023-10-02 23:47:46 +0300

    Update phone

---
+++ assert/js/phone-component.js
@@ -0,0 +1,29 @@
+const template =
+`
+    <link href="assert/css/phone.css" rel="stylesheet">
+    <div class = "container-widget-phone">
+        <div class = "widget-phone">
+            <div class = "widget-phone-header"></div>
+            <div class = "widget-phone-main">
+                <slot></slot>
+            </div>
+            <div class = "widget-phone-footer">
+                <img class="icon" src="assert/img/Icon10.svg">
+                <img class="icon" src="assert/img/Icon9.svg">
+                <img class="icon" src="assert/img/Icon8.svg">
+                <img class="icon" src="assert/img/Icon7.svg">
+                <img class="icon" src="assert/img/Icon6.svg">
+            </div>
+        </div>
+    </div>
+`
+class Phone extends HTMLElement {
+    constructor() {
+        super()
+        .attachShadow({mode:"open"})
+            .innerHTML = template;
+    }
+
+}
+
+customElements.define("widget-phone", Phone); // (2)
