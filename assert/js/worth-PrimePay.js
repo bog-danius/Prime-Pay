b2e6f9961bd7e7fdf2d0a1c6f1d219631977448b
bog-danius <Jenesius1998@gmail.com>
2023-10-09 23:17:19 +0300

    /worth

--- assert/js/worth-PrimePay.js
+++ assert/js/worth-PrimePay.js
@@ -6,17 +6,17 @@ class Worth extends HTMLElement {
         const text = this.getAttribute('text');
 
         this.innerHTML = `
-            <div class="container1-block flex-column gap_20 text_xs padding_45">
-                <div class="container1-block-image flex">
-                    <img src="assert/img/Image1.png">
-                </div>
-                <div class="container1-block-text flex ">
-                    <p class="container1-block1-logo text_lg">${name}</p>
-                    <p class="container1-block1-text ">${text}</p>
-                </div>
+        <div class="container1-block flex-column gap_20 text_xs padding_45">
+            <div class="container1-block-image flex">
+               <img src="assert/img/Image1.png">
             </div>
+                <div class="container1-block-text flex ">
+                   <p class="container1-block1-logo text_lg">${name}</p>
+                   <p class="container1-block1-text ">${text}</p>
+                </div>
+        </div>
         `
     }
 }
-customElements.define('worth-Primepay' , Worth);
+customElements.define('worth-PrimePay' , Worth);
 
